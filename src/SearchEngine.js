9a6a4c04488b370bccc3cca054529746e727bdb8
hannashapovalova <hannalova1991@gmail.com>
2022-09-08 19:51:27 +0100

    Add Bootstrap and footer

---
+++ src/SearchEngine.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+
+import './App.css';
+
+function SearchEngine() {
+  let [city, setCity] = useState(" ");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState(null);
+
+  function showTemperaure(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function handSubmit(event) {
+    event.preventDefault();
+    const apiKey = "238f6bbecd817b0849866bc3d0d8b987";
+    const unit = "metric";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(url).then(showTemperaure);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handSubmit}>
+      <input type="search" onChange={updateCity}></input>
+      <input type="submit" value="Search"></input>
+    </form>
+  );
+
+    return form;
+
+}
+
+export default SearchEngine;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SearchEngine from './SearchEngine';
 import reportWebVitals from './reportWebVitals';
+import './css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <main>
+      <a href="#" target="_blank" rel="nofollow">Kyiv</a>
+      <a href="#" target="_blank" rel="nofollow">Lisbon</a>
+      <a href="#" target="_blank" rel="nofollow">Paris</a>
+      <a href="#" target="_blank" rel="nofollow">San Francisco</a>
+    <SearchEngine />
+      
+    
+     
+
+    </main>
+    <footer>
+      <p>
+        This project was coded by 
+        <a href="#" target="_blank" rel="nofollow"> Anna Shapovalova</a>
+        and is
+        <a href="#" target="_blank" rel="nofollow"> open-sourced on GitHub </a>
+        and hosted on 
+        <a href="#" target="_blank" rel="nofollow"> Netlify</a>
+         .
+      </p>
+     </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
