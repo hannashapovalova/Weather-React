466c4427b32d0150d6c841b7e3ee10cf5d475f65
hannashapovalova <hannalova1991@gmail.com>
2022-09-09 23:20:16 +0100

    Correct

--- src/Footer.js
+++ src/Footer.js
@@ -6,15 +6,15 @@ export default function Footer() {
       <footer>
   <p>
     This project was coded by
-    <a href="https://www.linkedin.com/in/anna-shapovalova-63116b238/" target="_blank" rel="nofollow">
+    <a href="https://www.linkedin.com/in/anna-shapovalova-63116b238/" target="_blank" rel="noreferrer">
       Anna Shapovalova
     </a>
     and is
-    <a href="https://github.com/hannashapovalova/Weather-React" target="_blank" rel="nofollow">
+    <a href="https://github.com/hannashapovalova/Weather-React" target="_blank" rel="noreferrer">
       open-sourced on GitHub
     </a>
     and hosted on
-    <a href="https://dazzling-brioche-bd9aaa.netlify.app" target="_blank" rel="nofollow">
+    <a href="https://dazzling-brioche-bd9aaa.netlify.app" target="_blank" rel="noreferrer">
       Netlify
     </a>
     .
--- src/Weather.js
+++ src/Weather.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import CurrentDate from './CurrentDate';
-import axios from "axios";
+
 import './css/Weather.css';
-import { useState } from 'react';
+
 
 export default function Weather() {
 
-  let [city, setCity] = useState(" ");
-  let [weather, setWeather] = useState(null);
-
-
-    function showTemperaure(response) {
-    setWeather({
-      temperature: response.data.main.temp,
-      description: response.data.weather[0].description,
-      humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    });
-  }
   
-   function handSubmit(event) {
-    event.preventDefault();
-    const apiKey = "238f6bbecd817b0849866bc3d0d8b987";
-    const unit = "metric";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-    axios.get(url).then(showTemperaure);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
     return (
 
   <div className="container CurrentWeatherContainer">
