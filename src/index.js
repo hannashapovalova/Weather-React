1ddc80fec9292e036dac601831fa157e1862150d
hannashapovalova <hannalova1991@gmail.com>
2022-09-09 13:27:08 +0100

    Add Current Weather CSS

--- src/index.js
+++ src/index.js
@@ -16,23 +16,25 @@ root.render(
     <SearchEngine />
       
       <button type="button" className="btn btn-success">Current</button>
-      <div>
-        <h1>Lisbon</h1>
-        <h2>Monday 12:16</h2>
-        <h3>Clouds</h3>
-      </div>
-      <div>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <p>ICON</p>
-            </div>
-            <div className="col">
-              <p>Precipitation: 70%</p>
-              <p>Wind: 5km/h</p>
-            </div>
+      <div className="container CurrentWeatherContainer">
+
+        <div className="row">
+          <h1 className="CurrentCity">Lisbon</h1>
+          <h2 className="CurrentWeather">Monday 12:16</h2>
+          <h3 className="CurrentWeather">Clouds</h3>
+        </div>
+
+      
+        <div className="row">
+          <div className="col">
+            <p>ICON</p>
+          </div>
+          <div className="col">
+            <p className="CurrentWeather">Precipitation: 70%</p>
+            <p className="CurrentWeather">Wind: 5km/h</p>
           </div>
         </div>
+     
       </div>
       <div className="Forecast">
         <div className="container">
