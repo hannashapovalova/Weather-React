755523ad62ea527974f1ab6372a31172dfdc262c
hannashapovalova <hannalova1991@gmail.com>
2022-09-09 22:28:02 +0100

    Add Current Weather Function

--- src/Search.js
+++ src/Search.js
@@ -3,8 +3,8 @@ import './css/Search.css';
 
 export default function Search() {
     return (
-      <form className="SearchForm">
-        <input type="search" placeholder="Enter a city" className="SearchInput">
+      <form className="SearchForm" onSubmit={handSubmit}>
+        <input type="search" placeholder="Enter a city" className="SearchInput" onChange={updateCity}>
         </input>
         <input type="submit" value="Search" className="SeacrhButton">
         </input>
